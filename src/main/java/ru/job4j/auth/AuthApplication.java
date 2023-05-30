7b5d216a8e37621ad2af357bd14157befb4cbab7
lenarsharipov <lenarsharipov@gmail.com>
2023-05-30 17:56:23 +0300

    1. Spring Liquibase [#302330 #419622]

--- src/main/java/ru/job4j/auth/AuthApplication.java
+++ src/main/java/ru/job4j/auth/AuthApplication.java
@@ -1,12 +1,29 @@
 package ru.job4j.auth;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
-public class AuthApplication {
+public class AuthApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AuthApplication.class);
+	}
+
+	@Bean
+	public SpringLiquibase liquibase(DataSource ds) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+		liquibase.setDataSource(ds);
+		return liquibase;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
